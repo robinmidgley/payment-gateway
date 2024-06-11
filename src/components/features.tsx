2f4d07f77c36cfe3998eee2f0b0ed72affac29b8
robin <rpersonal71@gmail.com>
2024-06-11 02:17:07 +0100

    commit

--- src/components/features.tsx
+++ src/components/features.tsx
@@ -1,16 +1,38 @@
 import React from "react";
 
-import { ShieldCheckIcon } from "@heroicons/react/16/solid";
+import {
+  ShieldCheckIcon,
+  BoltIcon,
+  CubeTransparentIcon,
+} from "@heroicons/react/16/solid";
 
 export default function Features() {
   return (
-    <div className="grid grid-cols-4 gap-x-2">
-      <div className="border p-4">
-        <ShieldCheckIcon className="w-10 h-10 bg-black/10 rounded" />
+    <div className="grid grid-cols-3 gap-x-4">
+      <div className="flex flex-col gap-y-2 border p-4 rounded-lg hover:translate-y-[-10px] ease-in-out duration-500 hover:bg-blue-600 hover:text-white">
+        <ShieldCheckIcon className="w-10 h-10" />
+        <span className="text-xl font-medium">Safe and Secure</span>
+        <span>
+          Security is our number one priority, your transctions are safe and
+          private between you and your customers.
+        </span>
       </div>
-      <div className="border p-4">1</div>
-      <div className="border p-4">1</div>
-      <div className="border p-4">1</div>
+      <div className="flex flex-col gap-y-2 border p-4 rounded-lg hover:translate-y-[-10px] ease-in-out duration-500 hover:bg-blue-600 hover:text-white">
+        <BoltIcon className="w-10 h-10" />
+        <span className="text-xl font-medium">Lightning Fast</span>
+        <span>
+          Receive payments in no time and transfer between currencys
+          effortlessly.
+        </span>
+      </div>
+      <div className="flex flex-col gap-y-2 border p-4 rounded-lg hover:translate-y-[-10px] ease-in-out duration-500 hover:bg-blue-600 hover:text-white">
+        <CubeTransparentIcon className="w-10 h-10" />
+        <span className="text-xl font-medium">Easily Integrated</span>
+        <span>
+          Use our in-depth API to integrate WorldPay into your applications
+          seemlessly.
+        </span>
+      </div>
     </div>
   );
 }
