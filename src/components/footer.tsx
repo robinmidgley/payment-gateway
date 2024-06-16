c62360e5a2c63c1b8e41a66a72429cc747b1f401
robin <rpersonal71@gmail.com>
2024-06-16 13:45:24 +0100

    commit

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -3,7 +3,7 @@ import { GlobeAltIcon, CodeBracketIcon } from "@heroicons/react/24/outline";
 export default function Footer() {
   return (
     <div className="mt-16">
-      <div className="border-b pb-4">
+      <div className="border-b pb-8">
         <div className="grid md:grid-cols-5 grid-cols-2 gap-4">
           <div className="col-span-2 flex flex-col items-start">
             <div className="flex items-center gap-x-2">
@@ -16,43 +16,43 @@ export default function Footer() {
           </div>
           <div className="flex flex-col">
             <h3 className="font-semibold">Socials</h3>
-            <a href="#" className="mt-2">
+            <a href="#" className="mt-2 text-black/80 hover:text-black">
               Twitter
             </a>
-            <a href="#" className="mt-2">
+            <a href="#" className="mt-2 text-black/80 hover:text-black">
               YouTube
             </a>
-            <a href="#" className="mt-2">
+            <a href="#" className="mt-2 text-black/80 hover:text-black">
               Instagram
             </a>
           </div>
           <div className="flex flex-col">
             <h3 className="font-semibold">Contact</h3>
-            <a href="#" className="mt-2">
+            <a href="#" className="mt-2 text-black/80 hover:text-black">
               Email
             </a>
-            <a href="#" className="mt-2">
+            <a href="#" className="mt-2 text-black/80 hover:text-black">
               Phone
             </a>
-            <a href="#" className="mt-2">
+            <a href="#" className="mt-2 text-black/80 hover:text-black">
               Livechat
             </a>
           </div>
           <div className="flex flex-col">
             <h3 className="font-semibold">Links</h3>
-            <a href="#" className="mt-2">
+            <a href="#" className="mt-2 text-black/80 hover:text-black">
               Documentation
             </a>
-            <a href="#" className="mt-2">
+            <a href="#" className="mt-2 text-black/80 hover:text-black">
               Blog
             </a>
-            <a href="#" className="mt-2">
+            <a href="#" className="mt-2 text-black/80 hover:text-black">
               Careers
             </a>
           </div>
         </div>
       </div>
-      <div className="flex justify-between my-4">
+      <div className="flex justify-between my-6">
         <div>&#169; All rights reserved</div>
       </div>
     </div>
--- src/components/pricing.tsx
+++ src/components/pricing.tsx
@@ -13,10 +13,10 @@ const PricingPlan: React.FC<PricingPlanProps> = ({
   items,
   price,
 }) => (
-  <div className="border p-6 rounded-lg">
+  <div className="border p-8 rounded-lg">
     <div className="font-semibold text-4xl">{title}</div>
-    <div className="border-b py-3 text-black/60">{description}</div>
-    <div className="flex flex-col mt-3 gap-y-2">
+    <div className="border-b py-4 text-black/60">{description}</div>
+    <div className="flex flex-col mt-4 gap-y-2">
       {items.map((item, index) => (
         <div key={index} className="flex items-center gap-x-2">
           <CheckIcon className="w-5 h-5 text-blue-500" />
@@ -24,11 +24,11 @@ const PricingPlan: React.FC<PricingPlanProps> = ({
         </div>
       ))}
     </div>
-    <div className="flex my-8">
+    <div className="flex my-8 gap-x-1 items-center">
       <div className="text-2xl">{price}</div>
       <div className="text-sm">/mo</div>
     </div>
-    <button className="bg-blue-500 text-white w-full py-2 rounded hover:bg-blue-600 ease-in-out duration-200 text-xl font-medium">
+    <button className="bg-blue-500 text-white w-full py-2 rounded hover:bg-blue-600 ease-in-out duration-200 text-lg font-medium">
       Get it now
     </button>
   </div>
@@ -43,7 +43,7 @@ export default function Pricing() {
           A package fit for everyone, from individuals to large companies.
         </div>
       </div>
-      <div className="grid gap-y-4 grid-cols-1 md:grid-cols-3 gap-x-4 mt-8">
+      <div className="grid gap-y-4 grid-cols-1 md:grid-cols-3 gap-x-12 mt-8">
         <PricingPlan
           title="Starter"
           description="Great for individuals with small scale projects"
@@ -51,7 +51,7 @@ export default function Pricing() {
             "Custom payment links",
             "Currency conversion",
             "API access",
-            "Item 4",
+            "24/7 email support",
           ]}
           price="£24.99"
         />
@@ -68,12 +68,12 @@ export default function Pricing() {
         />
         <PricingPlan
           title="Enterprise"
-          description="A scalable solution for bigger companies"
+          description="A scalable and custom solution for larger businesses"
           items={[
             "Everything in Business",
             "Priority support",
             "Setup multiple accounts",
-            "Item 4",
+            "Dedicated account manager",
           ]}
           price="£99.99+"
         />
